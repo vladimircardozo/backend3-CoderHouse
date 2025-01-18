20dac3366f78b773e363d81f36846099384bd27d
vladimircardozo <vladimircardozo110@gmail.com>
2025-01-18 19:04:40 -0300

    socket/app

--- app.js
+++ app.js
@@ -1,21 +1,50 @@
-import express from 'express';
+import express from "express";
 import dotenv from "dotenv";
-import dbConnect from './src/utils/dbConnect.util.js'
-import appRouter from './src/routes/app.router.js'
+import { Server as socketIOserver } from "socket.io";
+import http from "http";
+import path from "path";
+import setupSocket from "./src/utils/socket.util.js";
+import dbConnect from "./src/utils/dbConnect.util.js";
+import appRouter from "./src/routes/app.router.js";
+import Product from "./src/data/mongo/models/product.model.js";
 
 dotenv.config();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 const app = express();
+const server = http.createServer(app);
+const io = new socketIOserver(server);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/", appRouter);
 
+app.get("products", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.render("index", { products });
+  } catch (error) {
+    console.error(
+      "Error al cargar los productos desde la base de datos:",
+      error
+    );
+    return res.status(500).json({ message: "Error al cargar los productos" });
+  }
+});
+
+setupSocket(io);
+
 dbConnect()
-    .then(() => {
-        app.listen(PORT, () => { console.log(`Servidor escuchando en el puerto ${PORT} 🚀`); });
-    }) .catch ((err) => {
-        console.error('Error connecting to MongoDB:', err);
-        process.exit(1);
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Servidor escuchando en el puerto ${PORT} 🚀`);
     });
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+    process.exit(1);
+  });
---
+++ src/utils/socket.util.js
@@ -0,0 +1,41 @@
+import Product from "../data/mongo/models/product.model.js";
+
+const setupSocket = (io) => {
+  io.on("connection", async (socket) => {
+    console.log("Nuevo cliente conectado 🙍‍♂️");
+
+    try {
+      const products = await Product.find();
+      socket.emit("actualizarProductos", products);
+    } catch (error) {
+      console.error("Error al obtener los productos", error);
+    }
+
+    socket.on("deleteProduct", async (productId) => {
+      try {
+        await Product.findByIdAndDelete(productId);
+        const products = await Product.find();
+        io.emit("actualizarProductos", products);
+      } catch (error) {
+        console.error("Error al eliminar el producto", error);
+      }
+    });
+
+    socket.on("addProduct", async (newProduct) => {
+      try {
+        const productToAdd = await Product.create(newProduct);
+        await productToAdd.save();
+        const products = await Product.find();
+        io.emit("actualizarProductos", products);
+      } catch (error) {
+        console.error("Error al agregar el producto", error);
+      }
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Cliente desconectado ❌");
+    });
+  });
+};
+
+export default setupSocket;
